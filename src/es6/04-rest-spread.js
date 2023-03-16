bbfd8de75bf1661df3247f4afd2fbc2a5d90262f
Horacio Sadaniswski <rhs9208@gmail.com>
2023-03-15 21:52:02 -0300

    commit clase 04

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,12 @@
+// desestructuracion de arrays
+
+let fruits = ['Apple', 'Banana'];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+//desestructuracion con objetos
+
+let user = {username: 'oscar', age: 34};
+let {username, age} = user;
+console.log(username, age);
+
