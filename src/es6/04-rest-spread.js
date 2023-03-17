c22d55d669f13776ef4aef9d90f8c672f2d210d3
Horacio Sadaniswski <rhs9208@gmail.com>
2023-03-17 19:38:56 -0300

    Commit hasta spread operator - rest

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -10,3 +10,20 @@ let user = {username: 'oscar', age: 34};
 let {username, age} = user;
 console.log(username, age);
 
+// Spread operator (...)
+
+let person = {name: 'oscar', age: 28};
+let country = 'mx';
+
+let data = {...person, country};
+console.log(data);
+
+// rest
+
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+
+sum (1, 5, 6, 7);
